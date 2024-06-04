63228784489aefa55d72d922a8817b459ef81a3d
Mohamed Fazil <mfazil@hello-robot.com>
2024-06-03 18:42:04 -0700

    improve gripper contact

--- hello_robot_stretch_3/stretch_mujoco.py
+++ hello_robot_stretch_3/stretch_mujoco.py
@@ -1,5 +1,28 @@
 import mujoco
 import mujoco.viewer
+from mujoco import MjModel, MjData
+import time
 
-model = mujoco.MjModel.from_xml_path("./scene.xml")
-mujoco.viewer.launch(model)
+class StretchMujocoSimulator:
+    def __init__(self):
+        self.model = mujoco.MjModel.from_xml_path("./scene.xml")
+        self.data = mujoco.MjData(self.model)
+    
+    def get_state_vector(self):
+        return {'time':self.data.time, 
+                'qpos':self.data.qpos, 
+                'qvel':self.data.qvel, 
+                'act': self.data.act}
+
+    def robot_control_callback(self, model: MjModel, data: MjData):
+        self.data = data
+        print(self.get_state_vector())
+
+    def run(self):
+        mujoco.set_mjcb_control(self.robot_control_callback)
+        mujoco.viewer.launch(self.model)
+
+
+if __name__ == "__main__":
+    stretch_mujoco_sim = StretchMujocoSimulator()
+    stretch_mujoco_sim.run()
